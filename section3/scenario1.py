f41ca4c45ab33998b0da46acb6348e20256dd992
jiahe.zhu0312 <jiahe.zhu0312@gmail.com>
2023-03-14 22:22:53 +0100

    resolved conflict

--- section3/scenario1.py
+++ section3/scenario1.py
@@ -7,21 +7,10 @@ from utils.visualization import visualize_network_with_stations_size
 
 flux_to_refueling = 0.003
 fuel_by_refueling = 45  # kg
-flux_to_refueling = 0.003
-fuel_by_refueling = 45  # kg
 
 
 def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
     w_x = x.copy()
-    res_size = dict()
-    res_h2day = dict()
-    res_profit = dict()
-    percentage_of_hydrogen_truck_list = [0.05, 0.1, 0.16, 0.267]
-    demand_treshold = [200000, 500000, 900000, 1500000]
-    cur_demand_sum = 0
-    for i, percentage_of_hydrogen_truck in enumerate(percentage_of_hydrogen_truck_list):
-        if i >= timesteps:
-            break
     res_size = dict()
     res_h2day = dict()
     res_profit = dict()
@@ -36,43 +25,8 @@ def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
             node: cn1.degree(node, weight="traffic flow")
             / np.ceil(cn1.degree(node) / 2)
             for node in x
-            for node in x
         }
         # dictionary with the estimated demand of H2 by day in kg by station
-        h2day_demand_all_nodes = {
-            node: x_val
-            * flux_by_station[node]
-            * flux_to_refueling
-            * percentage_of_hydrogen_truck
-            * fuel_by_refueling
-            for (node, x_val) in x.items()
-        }
-        h2station_all_nodes = {
-            node: 1 * (h2day >= 1000 and h2day <= 1800)
-            + 2 * (h2day > 1800 and h2day <= 2800)
-            + 3 * (h2day > 2800)
-            for (node, h2day) in h2day_demand_all_nodes.items()
-        }
-        h2day_all_nodes = {
-            node: (h2station_all_nodes[node] == 1) * min(1000, h2day_demand_all_nodes[node])
-            + (h2station_all_nodes[node] == 2) * min(2000, h2day_demand_all_nodes[node])
-            + (h2station_all_nodes[node] == 3) * min(4000, h2day_demand_all_nodes[node])
-            for node in h2station_all_nodes
-        }
-        h2profit_all_nodes = {
-            node: (h2station_all_nodes[node] == 1) * (h2day_all_nodes[node] - 900)
-            + (h2station_all_nodes[node] == 2) * (h2day_all_nodes[node] - 1600)
-            + (h2station_all_nodes[node] == 3) * (h2day_all_nodes[node] - 2400)
-            for node in h2station_all_nodes
-        }
-        for node in res_size:
-            cur_demand_sum -= res_h2day[node]
-            res_size[node] = h2station_all_nodes[node]
-            res_h2day[node] = h2day_all_nodes[node]
-            res_profit[node] = h2profit_all_nodes[node]
-            cur_demand_sum += res_h2day[node]
-        
-        
         h2day_demand_all_nodes = {
             node: x_val
             * flux_by_station[node]
@@ -120,12 +74,8 @@ def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
             node: 1 * (h2day >= 1000 and h2day <= 1800)
             + 2 * (h2day > 1800 and h2day <= 2800)
             + 3 * (h2day > 2800)
-            node: 1 * (h2day >= 1000 and h2day <= 1800)
-            + 2 * (h2day > 1800 and h2day <= 2800)
-            + 3 * (h2day > 2800)
             for (node, h2day) in h2day_demand_nodes.items()
         }
-        h2station_nodes = {k: v for k, v in h2station_nodes.items() if v != 0}
         h2station_nodes = {k: v for k, v in h2station_nodes.items() if v != 0}
         # dictionary with the demand answered by our model
         h2day_nodes = {
@@ -146,7 +96,6 @@ def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
             columns=["x", "y"],
         )
         while cur_demand_sum < demand_treshold[i] and len(h2profit_nodes.values()):
-            
             max_profit = max(h2profit_nodes.values())
             best_node = [
                 node
@@ -157,20 +106,16 @@ def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
             res_h2day[best_node] = h2day_nodes[best_node]
             res_profit[best_node] = h2profit_nodes[best_node]
             cur_demand_sum += h2profit_nodes[best_node]
-            res_size[best_node] = h2station_nodes[best_node]
-            res_h2day[best_node] = h2day_nodes[best_node]
-            res_profit[best_node] = h2profit_nodes[best_node]
-            cur_demand_sum += h2profit_nodes[best_node]
             df_dist = (
                 (df_coor.x - df_coor.loc[best_node, "x"]) ** 2
                 + (df_coor.y - df_coor.loc[best_node, "y"]) ** 2
             ).pow(0.5)
             df_nearest = df_dist[df_dist <= 20000].index.values
-            df_nearest = df_dist[df_dist <= 20000].index.values
             for key in df_nearest:
                 w_x.pop(key, None)
                 h2profit_nodes.pop(key, None)
             # remove neighbors from x and h2profit_nodes
+
         nx.set_node_attributes(cn1, 0, "S3P1_station_size")
         nx.set_node_attributes(cn1, res_size, "S3P1_station_size")
         nx.set_node_attributes(cn1, 0, "S3P1_h2day")
@@ -182,9 +127,9 @@ def scenario_1(cn1, x, timesteps=4, visualization=False, metrics=False):
             visualize_network_with_stations_size(cn1, 'S3P1_')
         if metrics:
             scenario_1_metrics(cn1)
+
     #do scenario in the for loop + demand decrease in size with time
     #question on scaling for the last date ?
-        
         
         
 
@@ -224,3 +169,7 @@ def scenario_1_metrics(roads):
     ax.set_ylabel("Number of stations")
     ax.tick_params(axis='x', rotation=45)
     fig.tight_layout()
+    
+    
+        
+
